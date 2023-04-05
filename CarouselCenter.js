aa116681a74a839536b89b6273905e4b93be1d9a
Phạm Ngọc Tuyên <tuyenpn@vtconline.com>
2023-04-05 22:40:17 +0700

    fix: duplicate show slider  active

--- CarouselCenter.js
+++ CarouselCenter.js
@@ -1,10 +1,9 @@
 class CarouselCenter {
   sliderList = undefined;
   sliderCurrentIndex = 0;
-  lastIndexSlider = undefined;
   lengthSlider = undefined;
   sliderContainer = undefined;
-
+  #SLIDE_CLASS = "slider-center__item";
   #ACTIVE_CLASS = "slider-center__item slider--item__active";
   #PREVIOUS_FIRST_CLASS = "slider-center__item slider--item__previous__first";
   #PREVIOUS_SECOND_CLASS = "slider-center__item slider--item__previous__second";
@@ -14,44 +13,47 @@ class CarouselCenter {
   constructor() {
     this.sliderList = document.querySelectorAll(".slider-center__item");
     this.sliderContainer = document.querySelector(".slider-center");
-
-    document.querySelector(".slider-center").style.height = this.sliderList[0].offsetHeight + "px"
-
-    if (this.sliderList && this.sliderContainer && this.sliderList.length >= 5) {
-      this.lastIndexSlider = this.sliderList.length - 1;
+    if (
+      this.sliderList &&
+      this.sliderContainer &&
+      this.sliderList.length >= 5
+    ) {
       this.lengthSlider = this.sliderList.length;
       this.setStyleSlider(this.sliderCurrentIndex);
+      document.querySelector(".slider-center").style.height =
+        this.sliderList[this.sliderCurrentIndex].offsetHeight + "px";
       this.sliderList.forEach((slide, index) => {
         slide.addEventListener("click", () => {
-          this.setStyleSlider(index)
-          this.setDotActive(index)
-        })
-      })
-      this.createDotsList()
-      this.setDotActive(this.sliderCurrentIndex)
+          this.setStyleSlider(index);
+          this.setDotActive(index);
+        });
+      });
+      this.createDotsList();
+      this.setDotActive(this.sliderCurrentIndex);
     }
   }
 
   createDotsList() {
-    const trackDots  = document.createElement("ul");
-    trackDots.className = "slider-center__dots"
-      this.sliderList.forEach((slide, index) => {
-      const dotItem = document.createElement("li")
-      dotItem.className = "dot__item"
+    const trackDots = document.createElement("ul");
+    trackDots.className = "slider-center__dots";
+    this.sliderList.forEach((slide, index) => {
+      const dotItem = document.createElement("li");
+      dotItem.className = "dot__item";
       dotItem.addEventListener("click", () => {
-        this.setStyleSlider(index)
-        this.setDotActive(index)
-      })
-      trackDots.appendChild(dotItem)
-    })
+        this.setStyleSlider(index);
+        this.setDotActive(index);
+      });
+      trackDots.appendChild(dotItem);
+    });
     this.sliderContainer.appendChild(trackDots);
   }
 
   setDotActive(currentIndex) {
-    const dotList = document.querySelectorAll(".dot__item") 
+    const dotList = document.querySelectorAll(".dot__item");
     dotList.forEach((dot, index) => {
-      dot.classList = currentIndex === index ? "dot__item dot__item--active" : "dot__item"
-    })
+      dot.classList =
+        currentIndex === index ? "dot__item dot__item--active" : "dot__item";
+    });
   }
 
   setStyleSlider(activeIndex) {
@@ -59,40 +61,60 @@ class CarouselCenter {
     const nextSecondIndex = this.getNextSliderIndex(2, activeIndex);
     const previousFirstIndex = this.getPreviousSliderIndex(1, activeIndex);
     const previousSecondIndex = this.getPreviousSliderIndex(2, activeIndex);
-
-    this.sliderList[activeIndex].classList = this.#ACTIVE_CLASS;
-    this.sliderList[nextFirstIndex].classList = this.#NEXT_FIRST_CLASS;
-    this.sliderList[nextSecondIndex].classList = this.#NEXT_SECOND_CLASS;
-    this.sliderList[previousFirstIndex].classList = this.#PREVIOUS_FIRST_CLASS;
-    this.sliderList[previousSecondIndex].classList = this.#PREVIOUS_SECOND_CLASS;
+    this.sliderList.forEach((slide, index) => {
+      switch (index) {
+        case activeIndex:
+          this.sliderList[index].classList = this.#ACTIVE_CLASS;
+          break;
+        case nextFirstIndex:
+          this.sliderList[index].classList = this.#NEXT_FIRST_CLASS;
+          break;
+        case nextSecondIndex:
+          this.sliderList[index].classList = this.#NEXT_SECOND_CLASS;
+          break;
+        case previousFirstIndex:
+          this.sliderList[index].classList = this.#PREVIOUS_FIRST_CLASS;
+          break;
+        case previousSecondIndex:
+          this.sliderList[index].classList = this.#PREVIOUS_SECOND_CLASS;
+          break;
+        default:
+          this.sliderList[index].classList = this.#SLIDE_CLASS;
+          break;
+      }
+    });
   }
 
   getPreviousSliderIndex(order, activeIndex) {
-    return activeIndex - order >= 0 ? activeIndex - order : this.lengthSlider - Math.abs(activeIndex - order);
+    return activeIndex - order >= 0
+      ? activeIndex - order
+      : this.lengthSlider - Math.abs(activeIndex - order);
   }
-  
+
   getNextSliderIndex(order, activeIndex) {
-    return activeIndex + order <= this.lastIndexSlider ? activeIndex + order : Math.abs(activeIndex + order) - this.lengthSlider;
+    return activeIndex + order <= this.lengthSlider - 1
+      ? activeIndex + order
+      : Math.abs(activeIndex + order) - this.lengthSlider;
   }
 
   previousSlider() {
-    this.sliderCurrentIndex--;
-    this.sliderCurrentIndex = this.sliderCurrentIndex < 0 ? this.lastIndexSlider : this.sliderCurrentIndex;
-    this.setDotActive(this.sliderCurrentIndex)
-    this.setStyleSlider(this.sliderCurrentIndex)
-  }
-  
-  nextSlider() {
-    this.sliderCurrentIndex++;
-    this.sliderCurrentIndex = this.sliderCurrentIndex % this.lengthSlider;
-    console.log(this.sliderCurrentIndex)
-    this.setDotActive(this.sliderCurrentIndex)
-    this.setStyleSlider(this.sliderCurrentIndex)
+    this.sliderCurrentIndex =
+      this.sliderCurrentIndex === 0
+        ? this.sliderList.length - 1
+        : --this.sliderCurrentIndex;
+    this.setDotActive(this.sliderCurrentIndex);
+    this.setStyleSlider(this.sliderCurrentIndex);
   }
 
-  setAutoPlay(){
+  nextSlider() {
+    this.sliderCurrentIndex = ++this.sliderCurrentIndex % this.lengthSlider;
+    this.setDotActive(this.sliderCurrentIndex);
+    this.setStyleSlider(this.sliderCurrentIndex);
+  }
+
+  setAutoPlay() {
     setInterval(() => {
-      this.nextSlider()
-    }, 2000);
+      this.nextSlider();
+    }, 2500);
   }
 }
